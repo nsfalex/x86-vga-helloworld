4d0c36ff591ce216d0f52f37ba84d787f13cc868
nsfalex <a.b.d.barinov@gmail.com>
2021-04-21 12:16:15 +0200

    Final code commit

--- dbg/build.sh
+++ dbg/build.sh
@@ -4,8 +4,8 @@ set -x -e
 
 rm hello.bin hello.sym obj/bootstrap.o obj/init.o || true
 
-i686-elf-as -o obj/bootstrap.o bootstrap.s && i686-elf-as -o obj/init.o init.s
-i686-elf-gcc -T linker.ld -o hello.bin -ffreestanding -O2 -nostdlib -lgcc -m32 obj/bootstrap.o obj/init.o
+i686-elf-as -o obj/hello.o hello.s
+i686-elf-gcc -T linker.ld -o hello.bin -ffreestanding -O2 -nostdlib -lgcc -m32 obj/*.o
 
 $(pwd)/dbg/generate-sym.sh hello.bin
 
